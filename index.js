b1712c27efe03c6e197cb2bb82cb087794b9061c
Kyran <kyran@gostelow.me>
2020-10-31 18:00:10 +1100

    Update index.js

--- index.js
+++ index.js
@@ -1,27 +1,60 @@
 import markdownIt from '/markdown-it-bundled/index.js' // TODO: Find a clean method
 import highlightJS from '/highlight.js-modified/src/highlight.js' // TODO: PR for making highlight.js compatible with ES6 modules by default
 import javascript from '/highlight.js-modified/src/languages/javascript.js'
-highlightJS.registerLanguage('javascript', javascript)
 
-const markdown = markdownIt({
+const moduleLocation = new URL(import.meta.url)
+const highlightOption = moduleLocation.searchParams.get('highlight') || 'none'
+const languages = {}
+	languages.light = [
+		'javascript'
+	]
+	languages.heavy = languages.light.concat([
+		'json',
+		'sql',
+		'rust',
+		'java'
+	])
+
+const languagePromise = Promise.all(languages[highlightOption]?.map(async language => {
+	import(`${moduleLocation.origin}/highlight.js-modified/src/languages/${language}.js`).then(module => {
+		highlightJS.registerLanguage(language, module.default)
+	})
+})) // I don't like this for performance reasons but oh well I guess
+
+const markdownOptions = {
 	allowHTML: false,
 	useHeadingIds: false,
 	useHeadingAnchors: false,
 	hrefMassager: undefined,
-	highlight: function (str, lang) {
-		if (lang && highlightJS.getLanguage(lang)) {
-			try {
-				return highlightJS.highlight(lang, str).value
-			} catch (__) {}
-		}
-
-		return '' // use external default escaping
+}
+if (highlightOption !== 'none') markdownOptions.highlight = (
+	str,
+	lang
+) => {
+	if (lang && highlightJS.getLanguage(lang)) {
+		try {
+			return highlightJS.highlight(lang, str).value
+		} catch (__) {}
 	}
-})
-let theme = document.createElement('style')
-document.head.appendChild(theme)
-fetch('hyper://7a9332a74279a911bd01e5d016fed0d790256637c3dc4423635b4ab3d9074880/highlight.js-modified/src/styles/monokai-sublime.css') // TODO: Find a better way to load this
-	.then(response => response.text())
-	.then(text => theme.innerHTML = text)
 
-export default markdown
+	return '' // use external default escaping
+}
+
+
+const markdown
+	= highlightOption === 'none'
+	? markdownIt(markdownOptions)
+	: async () => {
+		await languagePromise
+		return markdownIt(markdownOptions)
+	}
+
+const importCSS = () => {
+	let theme = document.createElement('style')
+	document.head.appendChild(theme)
+	fetch(`${moduleLocation.origin}/highlight.js-modified/src/styles/monokai-sublime.css`)
+		.then(response => response.text())
+		.then(text => theme.innerHTML = text)
+}
+
+export {markdown as default, importCSS}

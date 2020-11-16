59d3275d3468fb9405ea3552cdd49bc1639d8f10
Kyran <kyran@gostelow.me>
2020-11-16 23:40:14 +1100

    Get rid of silly hider method

--- plugins/hider.js
+++ plugins/hider.js
@@ -6,18 +6,9 @@ export default [
 		validate: () => true,
 
 		render: (tokens, idx) => {
-			const message =
-				tokens[idx].info.match(
-					/^[\s]*[spoil|warn|summary|hid|TW|trig]+[d|g|e]*[r|s|rs|ing|en|ed]*[s]*[:][\s]*(.*)$/
-				)?.[1] || "";
-
-			if (tokens[idx].nesting === 1) {
-				// opening tag
-				return `<details><summary>${message}</summary>\n`;
-			} else {
-				// closing tag
-				return "</details>\n";
-			}
+			return tokens[idx].nesting === 1
+				? `<details><summary>${tokens[idx].info}</summary>`
+				: `</details>`;
 		},
 	},
 ];
